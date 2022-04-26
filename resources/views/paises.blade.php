42992ad9e5dab4a088a1d19e4cc07a7d8fd70839
Cesar <ratedkhan5412@gmail.com>
2022-04-26 11:15:45 -0500

    arreglo paises

---
+++ resources/views/paises.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <title>Paises</title>
+</head>
+<body>
+    <center>
+        <h1>Paises de la región</h1>
+    </center>
+    <table class="table table-striped table-dark">
+        <thead>
+            <tr>
+                <th>Pais</th>
+                <th>Capital</th>
+                <th>Moneda</th>
+                <th>Población</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($paises as $pais => $infopai)
+            <tr>
+                <td>
+                    {{ $pais }}
+                </td>
+                <td>
+                    {{ $infopai["Capital"] }}
+                </td>
+                <td>
+                    {{ $infopai["Moneda"] }}
+                </td>
+                <td>
+                    {{ $infopai["Población"] }}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+
+        </tfoot>
+    </table>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//ruta de paises
+Route::get('paises', function()
+{
+    $paises=[
+        "Colombia" => [
+            "Capital" => "Bogotá",
+            "Moneda" => "Peso",
+            "Población" => 51.6,
+            "Ciudades" => [
+                "Medellín",
+                "Cali",
+                "Barranquilla"
+            ]
+        ],
+        "Peru" => [
+            "Capital" => "Lima",
+            "Moneda" => "Sol",
+            "Población" => 32.97,
+            "Ciudades" => [
+                "Lima",
+                "Cusco",
+                "Arequipa"
+            ]
+        ],
+        "Paraguay" =>[
+            "Capital" => "Asuncion",
+            "Moneda" => "Guarani paraguayo",
+            "Población" => 7.133,
+            "Ciudades" => [
+                "Ciudad de este",
+                "Luque",
+                "Encarnacion"
+            ]
+        ]
+    ];
+
+    //echo"<pre>";
+    //var_dump($paises);
+    //echo"</pre>";
+
+    //vista de paises
+    return view('paises')->with("paises" , $paises);
+
+});
